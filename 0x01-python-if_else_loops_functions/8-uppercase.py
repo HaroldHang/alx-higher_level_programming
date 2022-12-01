75c6b677bdfb016799531452c2e9ae0eebe1224c
HaroldHang <haroldadjahi@gmail.com>
2022-12-01 03:18:37 +0100

    pythons cond && hello

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-def uppercase(c):
-    if ord(c) > 64 and ord(c) < 91:
-        return True
-    else: 
-        return False
+def uppercase(str):
+    string = ""
+    for i in range(0, len(str)):
+        if ord(str[i]) > 64 and ord(str[i]) < 91:
+            string += str[i]
+        elif ord(str[i]) > 96 and ord(str[i]) < 124: 
+            string += chr(ord(str[i]) - 32)
+        else:
+            string += str[i]
+    return string
+
