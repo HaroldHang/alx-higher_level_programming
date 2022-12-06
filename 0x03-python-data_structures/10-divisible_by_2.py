37b5598a7032c940890b094ae779534ae49fe5f6
HaroldHang <haroldadjahi@gmail.com>
2022-12-06 03:50:35 +0100

    python data structures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 def divisible_by_2(my_list=[]):
     new_list = [False] * len(my_list)
-    for idx, num in enumerate(my_list):
-        if num % 2 == 0:
-            new_list[idx] = True
-    return new_list
+    if my_list:
+        for idx, num in enumerate(my_list):
+            if num % 2 == 0:
+                new_list[idx] = True
+        return new_list
+    else:
+        return new_list
